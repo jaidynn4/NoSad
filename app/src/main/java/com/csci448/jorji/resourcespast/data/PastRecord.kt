4c5371cab2ae2f2529a39083262d33c25e923089
JustinOrji <justinorji2@gmail.com>
2022-03-15 16:10:26 -0600

    Naive untested RecordScreen implementation

--- app/src/main/java/com/csci448/jorji/resourcespast/data/PastRecord.kt
+++ app/src/main/java/com/csci448/jorji/resourcespast/data/PastRecord.kt
@@ -1,36 +1,54 @@
 package com.csci448.jorji.resourcespast.data
 
 import android.provider.MediaStore
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.csci448.jorji.resourcespast.R
+import java.time.chrono.ChronoLocalDateTime
 import java.util.*
 
 
-class PastRecord(id:UUID=UUID.randomUUID(), title:String?, date:Date, textEntry:String?, audio:MediaStore.Audio?) {
+class PastRecord(id:UUID=UUID.randomUUID(), title:String?, date:Date, textTime:Date, audioTime:Date,textEntry:String?, audio:MediaStore.Audio?) {
     private val recordID = id
     private var recordTitle = title
     private var recordDate = date
     private var recordText = textEntry
     private var recordAudio = audio
+    private var recordTextTime = textTime
+    private var recordAudioTime = audioTime
 
     fun getID() : UUID{
         return recordID
     }
+    
+    @Composable
     fun getTitle() : String?{
         if(recordTitle != null){
             return recordTitle
         }
-        return null
+        return stringResource(id = R.string.record_title_placeholder)
     }
+    
     fun getDate() : Date{
         return recordDate
     }
-    fun getText() : String?{
-        if(recordText != null){
+
+    fun getTextTime() : Long {
+        return recordTextTime.time
+    }
+
+    fun getAudioTime() : Long {
+        return recordAudioTime.time
+    }
+
+    @Composable
+    fun getText(): String? {
+        if (recordText != null) {
             return recordText
         }
-        return null
+        return stringResource(id = R.string.record_text_entry_placeholder)
     }
+
     fun getAudio() : MediaStore.Audio?{
         if(recordAudio != null){
             return recordAudio
--- app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/PastRecordScreen.kt
+++ app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/PastRecordScreen.kt
@@ -1,4 +1,63 @@
 package com.csci448.jorji.resourcespast.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.csci448.jorji.resourcespast.R
+import com.csci448.jorji.resourcespast.data.PastRecord
+
 class PastRecordScreen {
+    @Composable
+    fun PastRecordScreen(list : List<PastRecord>?, onSelectRecord:(PastRecord) -> Unit){
+        if(list != null){
+            LazyColumn{
+                items(list){
+                    record -> RecordRow(record = record, onSelectRecord = onSelectRecord)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun RecordRow(record: PastRecord,onSelectRecord: (PastRecord) -> Unit){
+        Card(modifier =
+        Modifier
+            .clickable { onSelectRecord }
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+            .fillMaxWidth()
+        ){
+            Row{
+                Text(text = record.getDate().toString())
+                Column{
+                    record.getTitle()?.let{
+                        Row {
+                            Card {
+                                Text(text = it)
+                                Text(text = record.getTextTime().toString())
+                            }
+                        }
+                    }
+                    record.getAudio()?.let{
+                        Card {
+                            Text(text = stringResource(id = R.string.record_audio_display_text))
+                            Text(text = record.getAudioTime().toString())
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = true)
+    @Composable
+    private fun PreviewRecordList(){
+
+    }
 }
