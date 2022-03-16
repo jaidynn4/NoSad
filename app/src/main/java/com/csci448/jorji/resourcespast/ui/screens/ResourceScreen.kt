fa8dea65070b8629b52d6d82b25fe2d883b8e837
JustinOrji <justinorji2@gmail.com>
2022-03-15 22:29:27 -0600

    Resources Screen mostly implemented

--- app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/ResourceScreen.kt
+++ app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/ResourceScreen.kt
@@ -1,4 +1,123 @@
 package com.csci448.jorji.resourcespast.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+import com.csci448.jorji.resourcespast.R
+import com.csci448.jorji.resourcespast.data.Resource
+
 class ResourceScreen {
+    @Composable
+    fun ResourceScreen(list : List<Resource>?, onSelectResource : (Resource) -> Unit){
+        if(list != null) {
+            LazyColumn(
+                Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Text(
+                        text = stringResource(id = R.string.resources_screen_title),
+                        fontSize = 24.sp
+                    )
+                    Divider(thickness = 2.dp, color = colorResource(id = R.color.black))
+                    Spacer(Modifier.height(24.dp))
+                    Text(
+                        text = stringResource(id = R.string.resources_crisis_1),
+                        fontSize = 14.sp
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.resources_NSH),
+                        fontSize = 20.sp
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.resources_crisis_2),
+                        fontSize = 14.sp
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.resources_local_hotline).format("#-###-###-####"),
+                    )
+                    Spacer(Modifier.height(24.dp))
+                }
+                items(list) { resource ->
+                    ResourceRow(resource = resource, onSelectResource = onSelectResource)
+                }
+
+            }
+        }
+    }
+
+    @Composable
+    fun ResourceRow(resource: Resource, onSelectResource: (Resource) -> Unit){
+        Button(
+            onClick = { onSelectResource },
+            Modifier.padding(16.dp)
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(resource.getTitle(), fontSize = 16.sp)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(resource.getResourceBlurb())
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun PreviewResourceScreen(){
+        val list = mutableListOf<Resource>()
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_medical_title),
+                blurb = stringResource(id = R.string.resources_medical_blurb)
+            )
+        )
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_best_practices_title),
+                blurb = stringResource(id = R.string.resources_best_practices_blurb)
+            )
+        )
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_stats_title),
+                blurb = stringResource(id = R.string.resources_stats_blurb)
+            )
+        )
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_spiritual_title),
+                blurb = stringResource(id = R.string.resources_spiritual_blurb)
+            )
+        )
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_psychology_title),
+                blurb = stringResource(id = R.string.resources_psychology_blurb)
+            )
+        )
+        list.add(
+            Resource(
+                title= stringResource(id = R.string.resources_connect_title),
+                blurb = stringResource(id = R.string.resources_connect_blurb)
+            )
+        )
+        ResourceScreen(list = list, onSelectResource = {})
+
+    }
 }
