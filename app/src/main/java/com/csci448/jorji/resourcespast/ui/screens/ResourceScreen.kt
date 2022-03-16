f44a4562a2e8f20d45980955467c3446ef9c5b5b
JustinOrji <justinorji2@gmail.com>
2022-03-15 23:00:07 -0600

    Resources screen fully implemented

--- app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/ResourceScreen.kt
+++ app/src/main/java/com/csci448/jorji/resourcespast/ui/screens/ResourceScreen.kt
@@ -1,13 +1,15 @@
 package com.csci448.jorji.resourcespast.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
 import androidx.compose.ui.res.*
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import com.csci448.jorji.resourcespast.R
@@ -24,7 +26,8 @@ class ResourceScreen {
                 item {
                     Text(
                         text = stringResource(id = R.string.resources_screen_title),
-                        fontSize = 24.sp
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
                     )
                     Divider(thickness = 2.dp, color = colorResource(id = R.color.black))
                     Spacer(Modifier.height(24.dp))
@@ -45,11 +48,37 @@ class ResourceScreen {
                     Spacer(Modifier.height(8.dp))
                     Text(
                         text = stringResource(id = R.string.resources_local_hotline).format("#-###-###-####"),
+                        fontWeight = FontWeight.Bold
                     )
                     Spacer(Modifier.height(24.dp))
                 }
                 items(list) { resource ->
                     ResourceRow(resource = resource, onSelectResource = onSelectResource)
+                }
+                item{
+                    Text(text= stringResource(id = R.string.resources_socials), fontSize = 16.sp)
+                    Spacer(Modifier.height(8.dp))
+                    Row(
+                        Modifier.fillMaxWidth().padding(bottom = 20.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_facebook),
+                            contentDescription = stringResource(id = R.string.resources_fb)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_instagram),
+                            contentDescription = stringResource(id = R.string.resources_insta)
+                        )
+                        Image(painter = painterResource(
+                            id = R.drawable.icon_twitter),
+                            contentDescription = stringResource(id = R.string.resources_tw)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_reddit),
+                            contentDescription = stringResource(id = R.string.resources_red)
+                        )
+                    }
                 }
 
             }
